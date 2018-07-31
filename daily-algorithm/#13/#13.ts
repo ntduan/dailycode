d010418afcc27319c1384e6e431f94ccd62806d7
yznt <nsso@foxmail.com>
2018-07-31 13:22:12 +0800

    #30

--- daily-algorithm/#13/#13.ts
+++ daily-algorithm/#13/#13.ts
@@ -1,6 +1,34 @@
 /**
  * 暴力穷举
  */
+// const sub = (str, l) => {
+//   let maxLength = l;
+//   let result = [];
+//   for (let i = 0; i < str.length; i++) {
+//     for (let j = i; j < str.length; j++) {
+//       if (j + 1 < maxLength) continue;
+//       const sub = str.substring(i, i + j + 1);
+//       const subChar = {};
+//       for (let k = 0; k < sub.length; k++) {
+//         if (subChar[sub[k]]) {
+//           subChar[sub[k]] += 1;
+//         } else {
+//           subChar[sub[k]] = 1;
+//         }
+//       }
+//       const d = Object.keys(subChar).length;
+//       if (d === l) {
+//         if (sub.length > maxLength) {
+//           maxLength = sub.length;
+//           result = [sub];
+//         } else if (sub.length === maxLength) {
+//           result.push([sub]);
+//         }
+//       }
+//     }
+//   }
+//   return result;
+// };
 const sub = (str, l) => {
   let maxLength = l;
   let result = [];
---
+++ daily-algorithm/#30/#30.ts
@@ -0,0 +1,22 @@
+const walls = (nums: number[]) => {
+  const leftWall = nums.map(i => 0)
+  const rightWall = nums.map(i => 0)
+
+  let currentLeftMax = 0
+  for(let i = 0; i < leftWall.length; i++) {
+    currentLeftMax = Math.max(currentLeftMax, nums[i])
+    leftWall[i] = currentLeftMax
+  }
+
+  let currentRightMax = 0
+  for(let i = rightWall.length - 1; i >= 0; i--) {
+    currentRightMax = Math.max(currentRightMax, nums[i])
+    rightWall[i] = currentRightMax
+  }
+
+  let water = 0
+  for(let i = 0; i < nums.length - 1; i++) {
+    water += Math.min(leftWall[i], rightWall[i]) - nums[i]
+  }
+  return water
+}
