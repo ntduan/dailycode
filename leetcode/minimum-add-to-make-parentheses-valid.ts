ede7272f5163b50e18d8839e07cf101d2ba5e4cd
yznt <nsso@foxmail.com>
2018-10-15 19:28:06 +0800

    minAddToMakeValid

--- leetcode/minimum-add-to-make-parentheses-valid.ts
+++ leetcode/minimum-add-to-make-parentheses-valid.ts
@@ -4,21 +4,44 @@
 
 /**
  * 栈
-*/
+ */
+// var minAddToMakeValid = function(S) {
+//   if (!S.length) return 0;
+//   const matchMap = {
+//     ')': '(',
+//   };
+//   const stack = [];
+//   let i = 0;
+//   while (i < S.length) {
+//     if (stack[stack.length - 1] && matchMap[S[i]] === stack[stack.length - 1]) {
+//       stack.pop();
+//     } else {
+//       stack.push(S[i]);
+//     }
+//     i += 1;
+//   }
+//   return stack.length;
+// };
+
+/**
+ * O(1) 空间
+ */
 var minAddToMakeValid = function(S) {
   if (!S.length) return 0;
-  const matchMap = {
-    ')': '(',
-  };
-  const stack = [];
+
+  let count = 0;
+  let stack = 0;
   let i = 0;
   while (i < S.length) {
-    if (stack[stack.length - 1] && matchMap[S[i]] === stack[stack.length - 1]) {
-      stack.pop();
+    if (S[i] === '(') {
+      stack++;
+    } else if (!stack) {
+      // 如果是 ')' 则压入栈，不再匹配
+      count++;
     } else {
-      stack.push(S[i]);
+      stack--;
     }
     i += 1;
   }
-  return stack.length;
+  return count + stack;
 };
